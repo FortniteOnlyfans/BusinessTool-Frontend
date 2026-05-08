396e38d7ee552ca40615f1b4949224044243e6b9
Julian Hohenhausen <v22julian@gmail.com>
2026-05-08 17:17:20 +0200

    react project setup

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+import './App.css'
+import {Typography, Button, Box, Stack} from "@mui/material";
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            width="100%"
+        >
+            <Stack spacing={2} alignItems="center">
+                <Typography variant="h4">Hello World</Typography>
+
+                <Button
+                    variant="outlined"
+                    onClick={() => setCount((count) => count + 1)}
+                >
+                    Count is {count}
+                </Button>
+            </Stack>
+        </Box>
+    )
+}
+
+export default App
