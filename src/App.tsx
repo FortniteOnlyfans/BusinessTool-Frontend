515dbfb7ddf767e2b21a82af8f2f1c6bc62e09a1
v22 <v22julian@gmail.com>
2026-05-14 13:56:31 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
             width="100%"
         >
             <Stack spacing={2} alignItems="center">
-                <Typography variant="h4">Hello World</Typography>
+                <Typography variant="h4">Hello Test</Typography>
 
                 <Button
                     variant="outlined"
